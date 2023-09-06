6c260205c78d2848dcb5b418038eca495caea57f
Huang Chen's lab <135127832+ChenHuangMUST@users.noreply.github.com>
2023-09-06 20:55:43 +0800

    Add files via upload

---
+++ download_fpkm.R
@@ -0,0 +1,17 @@
+setwd("D:/tcga/tcga/somatic/methylation//")
+library(downloader)
+tcga_list<- c( "TCGA-BRCA", "TCGA-COAD", "TCGA-HNSC", "TCGA-KICH", "TCGA-KIRC", "TCGA-KIRP", "TCGA-LIHC","TCGA-LUAD", "TCGA-LUSC", "TCGA-PRAD", "TCGA-STAD", "TCGA-THCA", "TCGA-UCEC")
+
+#load("D:/tcga/psm_sample/top200/top200.Rdata")
+
+#https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-BRCA.survival.tsv
+for (i in 1:13){
+  tcga<-tcga_list[i]
+  download(paste("https://gdc-hub.s3.us-east-1.amazonaws.com/download/",tcga,".methylation450.tsv.gz",sep = ""),
+           paste(tcga,".masked_cnv.tsv.gz",sep = ""), mode = "wb")
+}
+#https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-LIHC.htseq_fpkm.tsv.gz
+###cnv download
+#https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-PRAD.masked_cnv.tsv.gz
+#https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-LUAD.mutect2_snv.tsv.gz
+#https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-LIHC.methylation450.tsv.gz
